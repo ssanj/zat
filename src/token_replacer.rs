896e9da14c05fd91a072819b0dd2b53e7cad6653
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-10-17 07:42:58 +1100

    wip

--- src/token_replacer.rs
+++ src/token_replacer.rs
@@ -1,3 +1,13 @@
+#[derive(Debug, Clone)]
+pub struct Token(String);
+
+impl AsRef<str> for Token {
+  fn as_ref(&self) -> &str {
+      &self.0.as_str()
+  }
+}
+
 pub trait TokenReplacer {
-  fn replace_token(token: &str) -> String;
+  fn replace_token<T>(token: T) -> String where
+    T: AsRef<str>;
 }
