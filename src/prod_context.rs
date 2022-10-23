6431f31779a7bf8998ae3c97c67ecaa40c27e545
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-10-24 09:18:19 +1100

    wip: move out TemplateTokenProvider

--- src/prod_context.rs
+++ src/prod_context.rs
@@ -38,14 +38,6 @@ impl ArgSupplier for Cli {
   }
 }
 
-struct UnimplementedType;
-
-impl ArgSupplier for UnimplementedType {
-  fn get_args(&self) -> Args {
-    todo!()
-  }
-}
-
 
 pub struct DefaultUserConfigProvider {
   arg_supplier: Box<dyn ArgSupplier>
@@ -99,6 +91,12 @@ impl UserConfigProvider for DefaultUserConfigProvider {
 
 struct DefaultTemplateTokenProvider;
 
+impl DefaultTemplateTokenProvider {
+  pub fn new() -> Self {
+    Self
+  }
+}
+
 // TODO: Should we move the file checks to a fake file system?
 // That would make it easier to test and lead to more reuse of code
 impl TemplateTokenProvider for DefaultTemplateTokenProvider {
@@ -136,7 +134,9 @@ impl TemplateTokenProvider for DefaultTemplateTokenProvider {
 #[cfg(test)]
 mod tests {
 
-  use super::*;
+  use crate::template_token_provider;
+
+use super::*;
   use tempfile::TempDir;
 
   struct TestArgs{
@@ -237,80 +237,80 @@ mod tests {
     }
   }
 
-  // #[test]
-  // fn tokens_are_empty_if_variable_file_does_not_exist() {
-  //   let target_dir = TempDir::new().unwrap();
-  //   let template_dir = TempDir::new().unwrap();
+  #[test]
+  fn tokens_are_empty_if_variable_file_does_not_exist() {
+    let target_dir = TempDir::new().unwrap();
+    let template_dir = TempDir::new().unwrap();
 
-  //   let template_dir_path = template_dir.path().display().to_string();
-  //   let target_dir_path = target_dir.path().display().to_string();
+    let template_dir_path = template_dir.path().display().to_string();
+    let target_dir_path = target_dir.path().display().to_string();
 
-  //   drop(target_dir);
+    drop(target_dir);
 
-  //   let prod = Prod::new();
+    let template_token_provider = DefaultTemplateTokenProvider::new();
 
-  //   let user_config = UserConfig {
-  //     template_dir: TemplateDir::new(&template_dir_path),
-  //     target_dir: TargetDir::new(&target_dir_path),
-  //     ignores: Ignores::default()
-  //   };
+    let user_config = UserConfig {
+      template_dir: TemplateDir::new(&template_dir_path),
+      target_dir: TargetDir::new(&target_dir_path),
+      ignores: Ignores::default()
+    };
 
-  //   let tokens = prod.get_tokens(user_config).expect("Expected to get tokens");
-  //   assert!(tokens.tokens.is_empty())
-  // }
+    let tokens = template_token_provider.get_tokens(user_config).expect("Expected to get tokens");
+    assert!(tokens.tokens.is_empty())
+  }
 
-  // #[test]
-  // fn tokens_are_loaded_from_variable_file() {
-  //   let target_dir = TempDir::new().unwrap();
-  //   let template_dir = TempDir::new().unwrap();
+  #[test]
+  fn tokens_are_loaded_from_variable_file() {
+    let target_dir = TempDir::new().unwrap();
+    let template_dir = TempDir::new().unwrap();
 
-  //   let template_dir_path = template_dir.path().display().to_string();
-  //   let target_dir_path = target_dir.path().display().to_string();
-  //   let variable_file_path = template_dir.path().join(VariableFile::PATH);
+    let template_dir_path = template_dir.path().display().to_string();
+    let target_dir_path = target_dir.path().display().to_string();
+    let variable_file_path = template_dir.path().join(VariableFile::PATH);
 
-  //   let mut variable_file = File::create(variable_file_path).unwrap();
+    let mut variable_file = File::create(variable_file_path).unwrap();
 
-  //   drop(target_dir);
+    drop(target_dir);
 
-  //   let variables_config = r#"
-  //     [
-  //       {
-  //         "variable_name": "project",
-  //         "description": "Name of project",
-  //         "prompt": "Please enter your project name",
-  //             "filters": [
-  //               {
-  //                 "name":"python",
-  //                 "filter": "Snake"
-  //               },
-  //               { "name": "Command",
-  //                 "filter": "Pascal"
-  //               }
-  //             ]
-  //       },
-  //       {
-  //         "variable_name": "plugin_description",
-  //         "description": "Explain what your plugin is about",
-  //         "prompt": "Please enter your plugin description"
-  //       }
-  //     ]
-  //   "#;
+    let variables_config = r#"
+      [
+        {
+          "variable_name": "project",
+          "description": "Name of project",
+          "prompt": "Please enter your project name",
+              "filters": [
+                {
+                  "name":"python",
+                  "filter": "Snake"
+                },
+                { "name": "Command",
+                  "filter": "Pascal"
+                }
+              ]
+        },
+        {
+          "variable_name": "plugin_description",
+          "description": "Explain what your plugin is about",
+          "prompt": "Please enter your plugin description"
+        }
+      ]
+    "#;
 
-  //   writeln!(&mut variable_file, "{}", variables_config).unwrap();
+    writeln!(&mut variable_file, "{}", variables_config).unwrap();
 
-  //   let prod = Prod::with_args_supplier(Box::new(UnimplementedType));
+    let template_config_provider = DefaultTemplateTokenProvider::new();
 
-  //   let user_config = UserConfig {
-  //     template_dir: TemplateDir::new(&template_dir_path),
-  //     target_dir: TargetDir::new(&target_dir_path),
-  //     ignores: Ignores::default()
-  //   };
+    let user_config = UserConfig {
+      template_dir: TemplateDir::new(&template_dir_path),
+      target_dir: TargetDir::new(&target_dir_path),
+      ignores: Ignores::default()
+    };
 
-  //   let tokens = prod.get_tokens(user_config).expect("Expected to get tokens");
-  //   assert_eq!(tokens.tokens.len(), 2);
+    let tokens = template_config_provider.get_tokens(user_config).expect("Expected to get tokens");
+    assert_eq!(tokens.tokens.len(), 2);
 
-  //   drop(variable_file);
-  // }
+    drop(variable_file);
+  }
 
 
 }
