8337b5b953f299dd0942c3691d449d50a42bf806
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2023-09-17 15:20:04 +1000

    add comments

--- src/main.rs
+++ src/main.rs
@@ -78,17 +78,19 @@ fn run_zat() {
   use crate::enriched_template_file_processor::EnrichedTemplateFileProcessor;
   use crate::DefaultEnrichedTemplateFileProcessor;
 
+  // Verifies that the source dir exists, and the destination does not and handles ignores (defaults and supplied).
+  // Basically everything from the cli config.
   let config_provider = DefaultUserConfigProvider::new();
   let user_config = config_provider.get_config().unwrap();
 
+  // Reads the .variables.prompt file into TemplateVariables
   let template_variable_provider = DefaultTemplateVariableProvider::new();
   let template_variables = template_variable_provider.get_tokens(user_config.clone()).unwrap();
 
+  // Ask for the user for the value of each variable
+  // Then verify all the variables supplied are correct
   let template_config_validator = DefaultTemplateConfigValidator::new();
   let template_variable_review = template_config_validator.validate(user_config.clone(), template_variables.clone());
-
-  let filter_applicator = ConvertCaseFilterApplicator;
-  let template_variable_expander = DefaultTemplateVariableExpander::with_filter_applicator(Box::new(filter_applicator));
 
   println!("config: {:?}", user_config);
   println!("variables: {:?}", template_variables);
@@ -96,10 +98,14 @@ fn run_zat() {
 
   match template_variable_review {
     TemplateVariableReview::Accepted(ValidConfig { user_variables, user_config: _ }) => {
+      // Expands variables names (VARIABLENAME__FILTER_NAME) for each variable supplied and mapped to their supplied values
+      let filter_applicator = ConvertCaseFilterApplicator;
+      let template_variable_expander = DefaultTemplateVariableExpander::with_filter_applicator(Box::new(filter_applicator));
       let expanded_variables = template_variable_expander.expand_filters(template_variables.clone(), user_variables);
+
+      // Surround expanded variable names with a token - the default is KEY_TOKEN ($)
       let key_tokenizer = DefaultKeyTokenizer::new(KEY_TOKEN);
       let tokenized_key_expanded_variables = key_tokenizer.tokenize_keys(expanded_variables.clone());
-      let aho_token_replacer = AhoCorasickTokenReplacer::new(tokenized_key_expanded_variables.clone());
 
       // TODO: This should be moved elsewhere
       let ignores: Vec<&str> =
@@ -109,20 +115,25 @@ fn run_zat() {
           .map(|i| i.as_str())
           .collect();
 
+      // Choose files to include by respecting ignores
       let file_chooser = regex_file_chooser::RegExFileChooser::new(&ignores).expect("Could not create file chooser");
       let file_traverser = WalkDirFileTraverser::new(Box::new(file_chooser));
       let files_to_process = file_traverser.traverse_files(&user_config.template_dir);
 
+      // Converts template files into enriched files that include replaced file name and content tokens
       let template_enricher = DefaultTemplateEnricher::new(user_config);
       let enriched_template_file_processor = DefaultEnrichedTemplateFileProcessor::with_defaults();
 
+      let aho_token_replacer = AhoCorasickTokenReplacer::new(tokenized_key_expanded_variables.clone());
       // TODO: Move this into an encapsulating module
       let zat_results: ZatActionX =
         files_to_process
           .into_iter()
-          .map(|tf| template_enricher.enrich(tf))
+          .map(|tf| template_enricher.enrich(tf)) // adds relative target file directory paths for each template
           .collect::<ZatResultX<Vec<EnrichedTemplateFile>>>()
           .and_then(|enriched_templates|{
+            // Writes out files and directories for each enriched template files while
+            // replacing any tokens in the file names and content
             enriched_template_file_processor.process_enriched_template_files(&enriched_templates, &aho_token_replacer)
           });
 
