6edbbb33915bea84d577d3edcc57e4dcf0c65852
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2023-10-18 10:00:51 +1100

    add tests for ShellHook post processor

--- src/post_processor/shell_hook.rs
+++ src/post_processor/shell_hook.rs
@@ -20,9 +20,38 @@ fn run_shell_hook(shell_hook: &str, user_config: &UserConfig) -> Result<(), ZatE
     Command::new(shell_hook)
       .arg::<&Path>(<TargetDir as AsRef<Path>>::as_ref(&user_config.target_dir).as_ref())
       .status()
-      .map_err(|e| ZatError::PostProcessingError(format!("Shell hook did not complete as expected: {}", e)))
+      .map_err(|e| ZatError::PostProcessingError(format!("Shell hook `{}` did not complete as expected: {}", shell_hook, e)))
       .map(|exit|{
         println!("Shell hook exited with {}", exit);
         ()
       })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_do_nothing_when_there_is_no_shell_hook() {
+        let config = UserConfig::new("", "");
+        assert_eq!(Ok(()), ShellHook.run(&config))
+    }
+
+    #[test]
+    fn should_fail_when_the_shell_hook_should_exist() {
+        let default_config = UserConfig::new("", "");
+
+        let config = UserConfig {
+          shell_hook_status: ConfigShellHookStatus::RunShellHook("some-script.sh".to_owned()),
+          ..default_config
+        };
+
+        match ShellHook.run(&config) {
+          Err(ZatError::PostProcessingError(error)) => {
+            println!("shell hook error: {}", &error);
+            assert!(error.starts_with("Shell hook `some-script.sh` did not complete as expected: No such file or directory"))
+          },
+          other => panic!("expected PostProcessingError but got: {:?}", other)
+        }
+    }
 }
