a8cff4978817527b996beade25fd20a7a8fbd362
Sanjiv Sahayam <sanjsmailbox@gmail.com>
2024-03-19 19:56:05 +1100

    refactor

--- src/command/process_templates.rs
+++ src/command/process_templates.rs
@@ -3,7 +3,7 @@ use crate::error::ZatAction;
 use crate::logging::{VerboseLogger, Logger};
 use crate::post_processor::{PostProcessingHook, ShellHook};
 use crate::processor::{ProcessTemplates as TemplateProcessing, DefaultProcessTemplates};
-use crate::templates::{DefaultTemplateVariableProvider, TemplateVariableProvider, TemplateVariables, UserChoices};
+use crate::templates::{DefaultTemplateVariableProvider, TemplateVariableProvider, TemplateVariables, UserChoices, ValidConfig};
 use crate::templates::{TemplateConfigValidator, DefaultTemplateConfigValidator};
 use crate::templates::TemplateVariableReview;
 use crate::token_expander::{ExpandFilters, DefaultExpandFilters};
@@ -43,29 +43,35 @@ impl ProcessTemplates {
 
     match template_variable_review {
       TemplateVariableReview::Accepted(vc) => {
-        VerboseLogger::log_user_supplied_variables(&user_config, &vc);
-        let user_variables = vc.user_variables;
-        let user_choices = UserChoices::new(vc.user_choices);
-        let expand_filters = DefaultExpandFilters::new();
-        let tokenized_key_expanded_variables = expand_filters.expand_filers(variables, user_variables);
-
-        VerboseLogger::expanded_tokens(&user_config, &tokenized_key_expanded_variables);
-        DefaultProcessTemplates.process_templates(&user_config, tokenized_key_expanded_variables, user_choices)?;
-
-        // Run post-processor if one exists
-        ShellHook.run(&user_config)?;
-
-        Logger::coloured(
-          &s!("{}{}{}",
-            Logger::info_str("Extracted template to '"),
-            &user_config.target_dir.path.as_str(),
-            Logger::info_str("'")
-          ))
+        Self::handle_accepted_variables(&user_config, variables, vc)?
       },
       TemplateVariableReview::Rejected => {
         Logger::warn("The user rejected the input review.")
       }
-    }
+    };
+
+    Ok(())
+  }
+
+  fn handle_accepted_variables(user_config: &UserConfig, variables: TemplateVariables, valid_config: ValidConfig) -> ZatAction {
+    VerboseLogger::log_user_supplied_variables(user_config, &valid_config);
+    let user_variables = valid_config.user_variables;
+    let user_choices = UserChoices::new(valid_config.user_choices);
+    let expand_filters = DefaultExpandFilters::new();
+    let tokenized_key_expanded_variables = expand_filters.expand_filers(variables, user_variables);
+
+    VerboseLogger::expanded_tokens(user_config, &tokenized_key_expanded_variables);
+    DefaultProcessTemplates.process_templates(user_config, tokenized_key_expanded_variables, user_choices)?;
+
+    // Run post-processor if one exists
+    ShellHook.run(user_config)?;
+
+    Logger::coloured(
+      &s!("{}{}{}",
+        Logger::info_str("Extracted template to '"),
+        &user_config.target_dir.path.as_str(),
+        Logger::info_str("'")
+      ));
 
     Ok(())
   }
